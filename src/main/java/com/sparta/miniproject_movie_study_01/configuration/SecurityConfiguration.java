4b8467c965790503de9f8e9536e85724b332efa6
pmjn1025 <pmjn1025@naver.com>
2022-08-17 22:33:55 +0900

    영현님과 공유

--- src/main/java/com/sparta/miniproject_movie_study_01/configuration/SecurityConfiguration.java
+++ src/main/java/com/sparta/miniproject_movie_study_01/configuration/SecurityConfiguration.java
@@ -58,7 +58,7 @@ public class SecurityConfiguration{
   @Bean
   @Order(SecurityProperties.BASIC_AUTH_ORDER)
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.cors().disable();
+//    http.cors().disable();
 
     http.csrf().disable()
 
