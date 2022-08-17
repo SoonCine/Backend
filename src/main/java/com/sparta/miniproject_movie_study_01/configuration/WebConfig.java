0ff96ee26cf3bc24ba2407433d189ba1c4a0ad09
ericjjung <ericjung@github.com>
2022-08-17 22:41:50 +0900

    브렌치 나눌거에요

--- src/main/java/com/sparta/miniproject_movie_study_01/configuration/WebConfig.java
+++ src/main/java/com/sparta/miniproject_movie_study_01/configuration/WebConfig.java
@@ -17,42 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
                     .exposedHeaders("Authorization","Refresh-Token")
                     .allowedHeaders("*")
                     // .exposedHeaders("Refresh-Token")
-                    .allowCredentials(true)//make client read header("jwt-token")
-            ;
+                    .allowCredentials(true);//make client read header("jwt-token")
+
 
         }
-
-    // 이걸 적용하니까 createdAt, modifiedAt 이상하게 나옴. 원인은 찾았으나
-    // 해결은 못함. 일단 이렇게 사용해보자.
-//    @Override
-//    public void addCorsMappings(CorsRegistry corsRegistry) {
-//
-//        corsRegistry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("*")
-//                .allowedHeaders("*")
-//                .exposedHeaders("*")
-//                .exposedHeaders("Authorization")
-//                .exposedHeaders("Refresh-Token")
-//                .maxAge(3000);
-//
-//
-//    }
-
-//    @Configuration
-//    public class WebMvcConfig implements WebMvcConfigurer {
-//        @Override
-//        public void addCorsMappings(CorsRegistry registry) {
-//            registry.addMapping("/**")
-//                    .allowedOrigins("http://localhost:3000","http://54.180.89.34")
-//                    .exposedHeaders("Authorization","Refresh-Token")
-//                    // .exposedHeaders("Refresh-Token")
-//                    .allowCredentials(true)//make client read header("jwt-token")
-//            ;
-//
-//        }
-//    }
-
 
 
 
